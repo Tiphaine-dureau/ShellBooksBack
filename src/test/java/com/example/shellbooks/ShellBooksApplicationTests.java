5db62b9f47ceeea38bb1e750f649c426b622a3eb
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-11-10 15:36:27 +0100

    First commit : initialized dependencies

---
+++ src/test/java/com/example/shellbooks/ShellBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shellbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
