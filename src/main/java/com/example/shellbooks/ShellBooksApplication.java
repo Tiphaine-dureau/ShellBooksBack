51fd2ff41c8560399df28ecf6b1b5d6c8469bd3d
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-11-10 17:17:56 +0100

    Configured CORS

--- src/main/java/com/example/shellbooks/ShellBooksApplication.java
+++ src/main/java/com/example/shellbooks/ShellBooksApplication.java
@@ -2,12 +2,26 @@ package com.example.shellbooks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ShellBooksApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShellBooksApplication.class, args);
+    }
+
+    @Bean
+    // Configuration du header Acces-control-allow-origin (CORS)
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
