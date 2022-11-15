7da0b588b2ceeb9bd0b9b0efe60b0fb27a26aeaf
Tiphaine du Réau <tiphaine.dureau@gmail.com>
2022-11-15 17:01:26 +0100

    Update Book Controller -> getmapping()

--- src/main/java/com/example/shellbooks/controllers/BookController.java
+++ src/main/java/com/example/shellbooks/controllers/BookController.java
@@ -1,23 +1,22 @@
 package com.example.shellbooks.controllers;
 
 import com.example.shellbooks.model.ViewBook;
+import com.example.shellbooks.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class BookController {
+    BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     @GetMapping("/books")
     public List<ViewBook> getBooks() {
-        List<ViewBook> viewBooks = new ArrayList<>();
-        ViewBook viewBook = new ViewBook("Harry Potter");
-        ViewBook viewBook2 = new ViewBook("Le seigneur des anneaux");
-
-        viewBooks.add(viewBook);
-        viewBooks.add(viewBook2);
-        return viewBooks;
+        return bookService.getBooks();
     }
 }
